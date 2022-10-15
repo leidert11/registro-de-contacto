f7fe8d94d1882f08cc24d3722073bd65f27c03c6
fanart13 <110786757+leidert11@users.noreply.github.com>
2022-10-15 09:02:39 -0500

    Update mian.js

--- mian.js
+++ mian.js
@@ -1,55 +1,75 @@
-class Persona{
-    constructor(nombre,apellido,documento){
-        this,nombre=nombre
-        this,apellido=apellido
-        this,documento=documento
-
+class Persona {
+    constructor(nombre, apellido, documento) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.documento = documento
     }
 }
 
-class Interfaz{
-    añadirPersona(persona){
-    const lista=document.getElementById('personas-list')
-    const elemento=document.createElement('div')
-    elemento.innerHTML=
-    `<div class="card text-center mb-4">
+class Interfaz {
+    añadirPersona(persona) {
+        const lista = document.getElementById('personas-list')
+        const elemento = document.createElement('div')
+        console.log(persona)
+        elemento.innerHTML =
+            `<div class="card text-center mb-4">
         <div class="card-body">
-        <strong>nombre</strong>nombre:
-        <strong>apéllido</strong>apellido:
-        <strong>documento</strong>documento:
-        <a href="#" class="btn btn-danger" name="eliminar">ELIMINAR</a>
-    </div>`
+            <strong>Nombre</strong>:${persona.nombre}<br>
+            <strong>apellido</strong>:${persona.apellido}<br>
+            <strong>Documento</strong>:${persona.documento}<br>
+            <a href="#" class="btn btn-danger" id="eliminar" name="eliminar">ELIMINAR</a>
+            </div>
+        </div>`
 
-    lista.appendChild(elemento)
-    this.dejarblanco()
-  }
-
- dejarblanco(){
-    document.getElementById('personas-form').reset()
- }
-
- eliminarPersonas(elemento){
-    if(elemento.name==='eliminar'){
-     elemento.parentElement.parentElement.remove()
+        lista.appendChild(elemento)
+        this.dejarblanco()
     }
- }
+
+    dejarblanco() {
+        document.getElementById('personas-form').reset()
+    }
+
+    eliminarPersona(elemento) {
+        if (elemento.name === 'eliminar') {
+            elemento.parentElement.parentElement.parentElement.remove()
+        }
+    }
+
+    mostrarmensaje(mensaje,tipo){
+        const div=document.createElement('div')
+        div.className=`alert alert-${tipo}`
+        div.appendChild(document.createTextNode(mensaje))
+        const contenedor= document.getElementsByClassName('container')
+        const app=document.getElementById('app')
+        contenedor[0].insertBefore(div,app)
+        setTimeout(function(){
+            div.remove()
+        },(2000))
+    }
 }
 
-//EVENTO QUE INTERACTUAN DESDE EL HTML
-document.getElementById('personas-form').addEventListener('submit',evento=>{
-  evento.preventDefault()
-  const nombre=document.getElementById('nombre').value
-  const apellido=document.getElementById('apellido').value
-  const documento=document.getElementById('documento').value
-  
+//EVENTOS QUE INTERACTUAL CON EL HTML
 
-  if (nombre===''|| apellido==='' || documento===''){
-    alert('campos vacios')
-  }else{
-    const persona=new Persona(nombre,apellido,documento)
+document.getElementById('personas-form').addEventListener('submit', e => {
+    e.preventDefault()
+    const nombre = document.getElementById('nombre').value
+    const apellido = document.getElementById('apellido').value
+    const documento = document.getElementById('documento').value
+
+    if(nombre==='' || apellido==='' || documento===''){
+        alert(`Campos vacios`)
+    }else{
+        const persona=new Persona(nombre,apellido,documento)
+        const interfaz=new Interfaz
+        interfaz.añadirPersona(persona)
+        // alert(`Persona creada correctamente`)
+        interfaz.mostrarmensaje('Contacto añadido correctamente','success')
+    }
+})
+
+document.getElementById('personas-list').addEventListener('click',evento=>{
     const interfaz=new Interfaz
-    interfaz.añadirPersona(persona)
-    alert('persona creada correctamente')
-
-  }
+    console.log(evento.target)
+    interfaz.eliminarPersona(evento.target)
+    interfaz.mostrarmensaje('Elemento eliminado','danger')
 })
